8b7f6caccfb3f656bca9b7ca92133de15f27277a
calvin-mcdowell <calvin.mcdowell@gmail.com>
2020-09-14 22:10:55 -0500

    Updated function to include specific rune page retrieval

--- tests/test_runes.py
+++ tests/test_runes.py
@@ -1,4 +1,5 @@
 from lcu_client.lcu_client import LcuClient
+from random import randint
 
 
 def test_rune_pages_retrieval():
@@ -8,5 +9,13 @@ def test_rune_pages_retrieval():
     client = LcuClient()
     runes = client.get_rune_pages()
 
-    assert len(runes) != 0
+    # Fail if no rune pages are returned
+    if len(runes) == 0:
+        assert False
+
+    # Validates that a random rune page can be returned
+    rand_page = runes[randint(0, len(runes))]
+    specific_page = client.get_rune_pages(name=rand_page['name'])
+
+    assert rand_page == specific_page
 
