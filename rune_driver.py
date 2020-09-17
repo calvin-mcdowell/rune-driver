aeadf427c39847339562c7f4dec986b9ed3f88c9
calvin-mcdowell <calvin.mcdowell@gmail.com>
2020-09-17 00:55:49 -0500

    Added import for RuneStorage class

--- rune_driver.py
+++ rune_driver.py
@@ -2,12 +2,13 @@
 https://github.com/calvin-mcdowell/rune-driver
 """
 from lcu_client.lcu_client import LcuClient
+from rune_storage.rune_storage import RuneStorage
 
 def main():
     """
     rune_driver.main() is just being used for class method verification until a main program loop is established
     """
-    pass
+    rune_storage = RuneStorage()
 
 if __name__ == '__main__':
     main()
